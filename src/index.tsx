a1253a46c8258d8843321cd73872c6da6e8e9f2a
dominon12 <youneedmax@gmail.com>
2022-03-14 12:16:19 +0100

    wrapp app component in redux-react Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "normalize.css";
 
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
