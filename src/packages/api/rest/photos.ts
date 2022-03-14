eed8d33578f82bf9048205e6881a9032c03a418b
dominon12 <youneedmax@gmail.com>
2022-03-14 11:50:25 +0100

    type photos endpoint

--- src/packages/api/rest/photos.ts
+++ src/packages/api/rest/photos.ts
@@ -1,17 +1,18 @@
 import { baseUrl } from "../config";
 import makeRequest from "../makeRequest";
+import Photo from "../../../types/api/Photo";
 
 const basePath = baseUrl + "photos/";
 
 export const list = (page?: number) => {
-  return makeRequest({
+  return makeRequest<Photo[]>({
     url: basePath,
     params: page ? { _page: page } : undefined,
   });
 };
 
 export const detail = (photoId: number) => {
-  return makeRequest({
+  return makeRequest<Photo>({
     url: basePath + photoId,
   });
 };
