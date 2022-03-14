08aa975b971bc6549b8b5a734d9505fad0c4ed72
dominon12 <youneedmax@gmail.com>
2022-03-14 12:15:12 +0100

    add thunk to load photos

--- src/redux/photos/thunks.ts
+++ src/redux/photos/thunks.ts
@@ -1,1 +1,19 @@
-export {};
+import { setPhotosData, setPhotosError, setPhotosLoading } from "./slice";
+import { AppDispatch } from "../types";
+import api from "../../packages/api";
+
+export const loadPhotos = (page: number) => async (dispatch: AppDispatch) => {
+  dispatch(setPhotosLoading(true));
+
+  const data = await api.photos.list(page);
+
+  dispatch(setPhotosLoading(false));
+
+  if (data.error) {
+    dispatch(setPhotosError(data.error));
+    return;
+  }
+
+  dispatch(setPhotosError(null));
+  dispatch(setPhotosData(data.data ?? []));
+};
