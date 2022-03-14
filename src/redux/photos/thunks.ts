2ff8dcdbf98183d6e5eae842d89e767993e6115f
dominon12 <youneedmax@gmail.com>
2022-03-14 19:54:45 +0100

    refactor photos api and thunks in order to be able to specify album id

--- src/redux/photos/thunks.ts
+++ src/redux/photos/thunks.ts
@@ -2,18 +2,19 @@ import { setPhotosData, setPhotosError, setPhotosLoading } from "./slice";
 import { AppDispatch } from "../types";
 import api from "../../packages/api";
 
-export const loadPhotos = (page: number) => async (dispatch: AppDispatch) => {
-  dispatch(setPhotosLoading(true));
+export const loadPhotos =
+  (page: number, albumId?: number) => async (dispatch: AppDispatch) => {
+    dispatch(setPhotosLoading(true));
 
-  const data = await api.photos.list(page);
+    const data = await api.photos.list({ page, albumId });
 
-  dispatch(setPhotosLoading(false));
+    dispatch(setPhotosLoading(false));
 
-  if (data.error) {
-    dispatch(setPhotosError(data.error));
-    return;
-  }
+    if (data.error) {
+      dispatch(setPhotosError(data.error));
+      return;
+    }
 
-  dispatch(setPhotosError(null));
-  dispatch(setPhotosData(data.data ?? []));
-};
+    dispatch(setPhotosError(null));
+    dispatch(setPhotosData(data.data ?? []));
+  };
